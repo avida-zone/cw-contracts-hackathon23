97af9dbc3596df6af0eb5393bd16ec9e2dd54cd5
bwty <whalelephant@users.noreply.github.com>
2023-04-22 17:33:03 +0800

    wip

--- ursa-demo/src/lib.rs
+++ ursa-demo/src/lib.rs
@@ -15,7 +15,7 @@ use ursa::cl::{
     SignatureCorrectnessProof,
 };
 
-pub fn data_dir() -> PathBuf {
+pub fn data_dir(dir: &str) -> PathBuf {
     let output = std::process::Command::new(env!("CARGO"))
         .arg("locate-project")
         .arg("--workspace")
@@ -24,12 +24,18 @@ pub fn data_dir() -> PathBuf {
         .unwrap()
         .stdout;
     let cargo_path = Path::new(std::str::from_utf8(&output).unwrap().trim());
-    let data_path = cargo_path.parent().unwrap().join("data");
+    let data_path = cargo_path.parent().unwrap().join("data/").join(dir);
     data_path
 }
 
+const CRED_SCHEMA_PATH: &str = "{}/credential_schema.json";
+const NON_CRED_SCHEMA_PATH: &str = "{}/non_credential_schema.json";
+const CRED_PUB_KEY: &str = "{}/credential_pub_key.json";
+const CRED_PRI_KEY: &str = "{}/credential_priv_key.json";
+const CRED_CORRECTNESS_PATH: &str = "{}/credential_correctness.json";
+
 pub fn get_issuer_setup_outputs(
-    file_prefix: String,
+    dir: String,
 ) -> (
     CredentialSchema,
     NonCredentialSchema,
@@ -37,34 +43,24 @@ pub fn get_issuer_setup_outputs(
     CredentialPrivateKey,
     CredentialKeyCorrectnessProof,
 ) {
-    let data_dir = data_dir();
+    let data_dir = data_dir(&dir);
     let path = data_dir.to_str().unwrap();
-    let schema_json =
-        fs::read_to_string(format!("{}/{}_credential_schema.json", path, file_prefix))
-            .expect("Unable to read file");
-    let schema: CredentialSchema = serde_json::from_str(&schema_json).expect("Unable to parse");
+    let schema_json = fs::read_to_string(format!(CRED_SCHEMA_PATH, file_prefix)).unwrap();
+    let schema: CredentialSchema = serde_json::from_str(&schema_json).unwrap();
 
-    let non_schema_json =
-        fs::read_to_string(format!("./{}_non_credential_schema.json", file_prefix))
-            .expect("Unable to read file");
-    let non_schema: NonCredentialSchema =
-        serde_json::from_str(&non_schema_json).expect("Unable to parse");
+    let non_schema_json = fs::read_to_string(format!(NON_CRED_SCHEMA_PATH, dir)).unwrap();
+    let non_schema: NonCredentialSchema = serde_json::from_str(&non_schema_json).unwrap();
 
-    let pk_json = fs::read_to_string(format!("./{}_credential_pub_key.json", file_prefix))
-        .expect("Unable to read file");
-    let pk: CredentialPublicKey = serde_json::from_str(&pk_json).expect("Unable to parse");
+    let pk_json = fs::read_to_string(format!(CRED_PUB_KEY, file_prefix)).unwrap();
+    let pk: CredentialPublicKey = serde_json::from_str(&pk_json).unwrap();
 
-    let priv_json = fs::read_to_string(format!("./{}_credential_priv_key.json", file_prefix))
-        .expect("Unable to read file");
-    let priv_key: CredentialPrivateKey = serde_json::from_str(&priv_json).expect("Unable to parse");
+    let priv_json = fs::read_to_string(format!(CRED_PRI_KEY, file_prefix)).unwrap();
+    let priv_key: CredentialPrivateKey = serde_json::from_str(&priv_json).unwrap();
 
-    let correctness_json = fs::read_to_string(format!(
-        "./{}_credential_key_correctness_proof.json",
-        file_prefix
-    ))
-    .expect("Unable to read file");
+    let correctness_json = fs::read_to_string(format!(CRED_CORRECTNESS_PATH, file_prefix)).unwrap();
+
     let correctness: CredentialKeyCorrectnessProof =
-        serde_json::from_str(&correctness_json).expect("Unable to parse");
+        serde_json::from_str(&correctness_json).unwrap();
 
     (schema, non_schema, pk, priv_key, correctness)
 }
@@ -72,7 +68,7 @@ pub fn get_issuer_setup_outputs(
 pub fn issuer_set_up(
     schema_attrs: Vec<&str>,
     non_schema_attrs: Vec<&str>,
-    file_prefix: String,
+    dir: String,
 ) -> (
     CredentialSchema,
     NonCredentialSchema,
@@ -95,31 +91,34 @@ pub fn issuer_set_up(
     let non_credential_schema = non_credential_schema_builder.finalize().unwrap();
     info!("non credential schema {:?}", non_credential_schema);
 
+    let data_dir = data_dir(&dir);
+    let path = data_dir.to_str().unwrap();
+
     // Credential definition
     let (credential_pub_key, credential_priv_key, cred_key_correctness_proof) =
         Issuer::new_credential_def(&credential_schema, &non_credential_schema, false).unwrap();
     info!("credential pub key {:?}", credential_pub_key);
 
     std::fs::write(
-        format!("{}_credential_schema.json", file_prefix),
+        format!(CRED_SCHEMA_PATH, &dir),
         serde_json::to_string_pretty(&credential_schema).unwrap(),
     )
     .unwrap();
 
     std::fs::write(
-        format!("{}_non_credential_schema.json", file_prefix),
+        format!("{}/{}_non_credential_schema.json", path, file_prefix),
         serde_json::to_string_pretty(&non_credential_schema).unwrap(),
     )
     .unwrap();
 
     std::fs::write(
-        format!("{}_credential_pub_key.json", file_prefix),
+        format!("{}/{}_credential_pub_key.json", path, file_prefix),
         serde_json::to_string_pretty(&credential_pub_key).unwrap(),
     )
     .unwrap();
 
     std::fs::write(
-        format!("{}_credential_priv_key.json", file_prefix),
+        format!("{}_credential_priv_key.json", path, file_prefix),
         serde_json::to_string_pretty(&credential_priv_key).unwrap(),
     )
     .unwrap();
