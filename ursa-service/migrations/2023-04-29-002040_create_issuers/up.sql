1519987596cfe555cbd4fbdae9fd0772860703bf
bwty <whalelephant@users.noreply.github.com>
2023-04-29 22:41:01 +0800

    move service to ursa-service repo

--- ursa-service/migrations/2023-04-29-002040_create_issuers/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE issuers (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR NOT NULL,
-  correctness TEXT NOT NULL,
-  privkey TEXT NOT NULL,
-  subproofreqparams TEXT NOT NULL
-)
--- ursa-service/migrations/2023-04-29-054043_create_holders/up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE credentials (
-  id SERIAL PRIMARY KEY,
-  contractaddr VARCHAR NOT NULL,
-  issuer VARCHAR NOT NULL,
-  walletaddr VARCHAR NOT NULL,
-  credsig TEXT NOT NULL,
-  credvalues TEXT NOT NULL
-)
